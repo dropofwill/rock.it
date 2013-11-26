5c38215afa2a18a4a97e1940ed3deada67f900c5
Will <whp3652@rit.edu>
2013-11-26 01:37:40 -0500

    implent slideToggle JQuery, laggy

--- js/script.js
+++ js/script.js
@@ -1,23 +1,16 @@
 var $doc = $(document),
 	$inputs = $(".btn--inputs"),
-	$signup_btn = $("input[name='sigup']"),
+	$signup_btn = $("input[name='signup']"),
 	$login_btn = $("input[name='login']"),
-	$signup_form = $("#signup_form"),
-	$login_form = $("#login_form"),
-	signup_open = false,
-	login_open = false;
+	$signup_form = $("#signup_form > .btn--inputs"),
+	$login_form = $("#login_form");
 
 $doc.ready(function() {
-	//$('.btn--inputs').hide();
+	$('.btn--inputs').hide();
 
 	$signup_btn.click(function(event){
-		if (signup_open === false) {
-			event.preventDefault();
-			$signup_form.preventDefault();
-			$signup_form.$inputs.fadeIn();
+			console.log('yolo');
+			$signup_form.slideToggle();
 		}
-		else {
-
-		}
-	});
+	);
 });

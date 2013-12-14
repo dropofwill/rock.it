76f74bf210667c48892684eb9fbfb0da1ce6b748
Will <whp3652@rit.edu>
2013-12-13 16:20:57 -0800

    Fix gitignore

--- rock_it_asp/rock_it_asp/Models/IdentityModels.cs
+++ rock_it_asp/rock_it_asp/Models/IdentityModels.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
 
 namespace rock_it_asp.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public virtual ICollection<Post> Posts { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
---
+++ rock_it_asp/rock_it_asp/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace rock_it_asp.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public bool IsLink { get; set; }
+    }
+
+    public class PostDBContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+}
